178ae1c562f39ee439fdb7733d86597803668008
AlexGekko88 <115148133+AlexGekko88@users.noreply.github.com>
2023-01-23 22:13:10 +0300

    add2

--- src/Main.java
+++ src/Main.java
@@ -29,66 +29,78 @@ public class Main {
 
             switch (operationNumber) {
                 case 1:
-                    System.out.println("Какую покупку вы ходите добавить? ");
-                    String inputPurchase = scan.nextLine();
-                    if (!inputPurchase.trim().equals("")) {
-                        purchases.add(capitalize(inputPurchase));
-                        System.out.println("Итого в списке покупок: " + purchases.size());
-                    }
+                    addProduct(purchases, scan);
                     break;
                 case 2:
                     purchasePrint(purchases);
                     break;
                 case 3:
-                    purchasePrint(purchases);
-                    System.out.println("Какую хотите удалить? Введите номер или название: ");
-                    String inputToDelete = scan.nextLine();
-                    int numberToDelete = -1;
-                    String purchaseToDelete = null;
-                    try {
-                        numberToDelete = (Integer.parseInt(inputToDelete.trim()) - 1); //определяем номер покупки к удалению
-                    } catch (NumberFormatException exception) {
-                        purchaseToDelete = inputToDelete; //если введена строка, то будем пытаться удалить ее
-                    }
-
-                    if (numberToDelete >= 0 && numberToDelete < purchases.size()) {
-                        String deleting = purchases.get(numberToDelete);
-                        purchases.remove(numberToDelete);
-                        System.out.println("Покупка '" + deleting + "' удалена.");
-                        purchasePrint(purchases);
-                    } else if (numberToDelete >= purchases.size() || (numberToDelete < 0 && purchaseToDelete == null)) {
-                        System.out.println("Покупки с таким номером не существует");
-                    } else if (purchaseToDelete != null) {
-                        try {
-                            purchaseToDelete = capitalize(purchaseToDelete);
-                            purchases.remove(purchases.indexOf(purchaseToDelete));
-                            System.out.println("Покупка '" + purchaseToDelete + "' удалена.");
-                            purchasePrint(purchases);
-                        } catch (IndexOutOfBoundsException exception) {
-                            System.out.println("Покупки с таким названием не существует");
-                        }
-                    }
+                    deleteProduct(purchases, scan);
                     break;
                 case 4:
-                    System.out.println("Введите текст для поиска: ");
-                    String inputToFind = scan.nextLine();
-                    boolean flag = true;
-                    for (int i = 0; i < purchases.size(); i++) {
-                        String itemLower = purchases.get(i).trim().toLowerCase();
-                        String toFindLower = inputToFind.trim().toLowerCase();
-                        if (itemLower.contains(toFindLower)) {
-                            System.out.println(i + 1 + ". " + purchases.get(i));
-                            flag = false;
-                        }
-                    }
-                    if (flag) {
-                        System.out.println("Таких покупок не нашлось");
-                    }
+                    searchProduct(purchases, scan);
 
                     break;
                 default:
                     System.out.println("Нет операции с таким номером.");
             }
+        }
+    }
+
+    public static void searchProduct(List<String> purchases, Scanner scan) {
+        System.out.println("Введите текст для поиска: ");
+        String inputToFind = scan.nextLine();
+        boolean flag = true;
+        for (int i = 0; i < purchases.size(); i++) {
+            String itemLower = purchases.get(i).trim().toLowerCase();
+            String toFindLower = inputToFind.trim().toLowerCase();
+            if (itemLower.contains(toFindLower)) {
+                System.out.println(i + 1 + ". " + purchases.get(i));
+                flag = false;
+            }
+        }
+        if (flag) {
+            System.out.println("Таких покупок не нашлось");
+        }
+    }
+
+    public static void deleteProduct(List<String> purchases, Scanner scan) {
+        purchasePrint(purchases);
+        System.out.println("Какую хотите удалить? Введите номер или название: ");
+        String inputToDelete = scan.nextLine();
+        int numberToDelete = -1;
+        String purchaseToDelete = null;
+        try {
+            numberToDelete = (Integer.parseInt(inputToDelete.trim()) - 1); //определяем номер покупки к удалению
+        } catch (NumberFormatException exception) {
+            purchaseToDelete = inputToDelete; //если введена строка, то будем пытаться удалить ее
+        }
+
+        if (numberToDelete >= 0 && numberToDelete < purchases.size()) {
+            String deleting = purchases.get(numberToDelete);
+            purchases.remove(numberToDelete);
+            System.out.println("Покупка '" + deleting + "' удалена.");
+            purchasePrint(purchases);
+        } else if (numberToDelete >= purchases.size() || (numberToDelete < 0 && purchaseToDelete == null)) {
+            System.out.println("Покупки с таким номером не существует");
+        } else if (purchaseToDelete != null) {
+            try {
+                purchaseToDelete = capitalize(purchaseToDelete);
+                purchases.remove(purchases.indexOf(purchaseToDelete));
+                System.out.println("Покупка '" + purchaseToDelete + "' удалена.");
+                purchasePrint(purchases);
+            } catch (IndexOutOfBoundsException exception) {
+                System.out.println("Покупки с таким названием не существует");
+            }
+        }
+    }
+
+    public static void addProduct(List<String> purchases, Scanner scan) {
+        System.out.println("Какую покупку вы ходите добавить? ");
+        String inputPurchase = scan.nextLine();
+        if (!inputPurchase.trim().equals("")) {
+            purchases.add(capitalize(inputPurchase));
+            System.out.println("Итого в списке покупок: " + purchases.size());
         }
     }
 
